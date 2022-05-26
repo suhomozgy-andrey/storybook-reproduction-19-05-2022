8545fa629119992198981be373af9a61f7b12e3a
Valentin Palkovič <valentin.palkovic.extern@elinvar.de>
2022-05-26 12:15:12 +0200

    Update package-lock.json and fix setup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,9 @@
-import "../src/input.css";
-
 export const parameters = {
-	actions: { argTypesRegex: "^on[A-Z].*" },
-	controls: {
-		matchers: {
-			color: /(background|color)$/i,
-			date: /Date$/
-		}
-	}
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/
+        }
+    }
 };
